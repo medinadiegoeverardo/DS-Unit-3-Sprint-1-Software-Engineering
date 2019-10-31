ea3272cad3c4203efd7eb85adb9ecc5df36e63be
medinadiegoeverardo <diegoedina.a@gmail.com>
2019-10-31 08:17:46 -0700

    class for functions

--- module2-oop-code-style-and-reviews/classes_lecture/op copy.py
+++ module2-oop-code-style-and-reviews/classes_lecture/op copy.py
@@ -4,42 +4,6 @@ Classes to represent games
 
 from random import random
 
-# class Game:
-
-#     """
-#     representation of game
-#     """
-
-#     fun_level = 10
-
-#     def __init__(self, player_1='Owen', player_2='Brandon'):
-#         self.rounds = 2
-#         self.steps = 5
-#         self.player_1 = player_1
-#         self.player_2 = player_2
-
-#     def print_players(self):
-#         print('{} is playing {}'.format(self.player_1, self.player_2))
-
-#     def add_rounds(self):
-#         self.rounds += 1
-
-#     def winner(self):
-#         """ 
-#         Randomly select and return winner
-#         """
-#         return self.player_1 if random() > 0.5 else self.player_2
-
-# class Tic(Game):
-    
-#     def __init__(self, rounds=10, steps=100, player_1='Diego', player_2='Robert'):
-#         super().__init__(rounds, player_1, player_2)
-
-#     def print_players(self):
-#         print('{} is playing TicTacToe with {}'.format(self.player_1, self.player_2))
-
-
-
 class Game:
     """
     General representation of an abstract game
--- module2-oop-code-style-and-reviews/ran_word_c/__init__.py
+++ module2-oop-code-style-and-reviews/ran_word_c/__init__.py
@@ -12,37 +12,51 @@ with open('/Users/diego/Documents/LambdaUnit3/rand_words_package/ran_words/words
 
 class Random():
 
+    """
+    For dataframe with custom shape,
+    input a variable inplace of entire words dictionary like so:
+        
+    first -> data = x.select_words_range(100)
+    then -> make_df_rows(10, data)
+    shape would be: (100, 1)
+
+    alternatively, input entire words dictionary (25,486 words) -> make_df_rows(10)
+    shape would be: (100, 255)
+    """
+
     def __init__(self, words):
         self.words = words
-
+        
     def select_words_range(self, number_of_words, first_word_number=0, last_word_number=None):
+        """
+        select number of words in combination and range. 
+        there are 25,486 words. Words starting with L will fall somewhere around 12,000
+        """
         numbers = []
         sentence = []
         if last_word_number == None:
             last_word_number = len(self.words)
-            """
-            randomly pick x numbers from here to here
-            """
             for i in range(0, number_of_words):
                 numbers.append(np.random.randint(first_word_number, last_word_number))
             for i in numbers:
                 sentence.append(self.words[i])
             return " ".join(sentence)
 
-    def make_df_rows(self, rows): # self is object with words
+    def make_df_rows(self, rows, words=None):
         """
-        This function is useful when the user wants to provide list of words
-        and separate them by number of rows
-
-        make_df_rows(10) yields 10 rows of x list
+        select rows for words list
         """
-        data_for_df = np.array_split(self.words, rows)
-        df = pd.DataFrame(data=data_for_df)
-        return df
+        if words == None:
+            data_for_df = np.array_split(self.words, rows)
+            return pd.DataFrame(data=data_for_df)
+        else:
+            word = words.split(" ")
+            data_for_df = np.array_split(word, rows)
+            return pd.DataFrame(data=data_for_df)
 
 # -----------------
 
-# attempt to give user more freedom to input dif files of words/letters
+# attempt to give user more freedom to input dif files of words
 
 
 # import pandas as pd
